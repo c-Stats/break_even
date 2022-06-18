a567864ecf32c98fb122b53c1354ef7430636be5
c-Stats <77307483+c-Stats@users.noreply.github.com>
2022-06-18 17:05:22 -0400

    Update break_even.R

--- break_even.R
+++ break_even.R
@@ -1,55 +1,41 @@
 require(microbenchmark)
 
-#One ticket, one play
-f <- function(prize){
-     
-     n <- rhyper(1, 6, 53, 6)     
-     return(c(n, prize[n + 1]))
-     
-}
- 
-#One ticket, keep playing if n = 2
-g <- function(prize){
-     
-     S <- 0
-     while(TRUE){
-         
-         x <- f(prize)
-         S <- S + x[2]
-         if(x[1] != 2){break}
-         
-     }
-     
-     return(S)
-     
- }
-
 #Play n times
 simulate <- function(n){
 
 	hits <- rhyper(n, 6, 53, 6)
 	cost <- 2 * n
 	prize <- c(0, 0, 2, 30, 140, 1750, 10^6) 
+	gains <- 0
 
-	final <- which(hits != 2)
-	S <- sum(prize[hits[final] + 1])
+	gains <- gains + sum(prize[hits + 1])
+	#Replay when hit == 2
+	n <- sum(hits == 2)
 
-	hits <- hits[-final]
-	S <- sum(hits) + sum(sapply(hits, function(x){g(prize)}))
+	while(n != 0){
 
-	return(S - cost)
+		hits <- rhyper(n, 6, 53, 6)
+		gains <- gains + sum(prize[hits + 1])
+		n <- sum(hits == 2)
+
+	}
+
+	return(gains - cost)
 	
 }
 
-#Simulation will run for approximately 1min
+#Simulation will run for approximately n mins
+n <- 10
 runtime <- microbenchmark(simulate(7696))
 
 mean_runtime <- mean(runtime$time) / 10^9
-n_runs <- round(60 / 0.004739061)
+n_runs <- round(n * 60 / 0.004739061)
 
+print(paste("Runtime: ", n, " minutes.", sep = ""), quote = FALSE)
 print(paste("Running ", n_runs, " times.", sep = ""), quote = FALSE)
 
 sims <- replicate(n_runs, simulate(7696))
 p_break_even <- sum(sims >= 0) / length(sims)
 
 print(paste("P(Break even) = ", p_break_even, sep = ""), quote = FALSE)
+
